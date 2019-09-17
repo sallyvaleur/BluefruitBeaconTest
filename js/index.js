b815aa190e041421a583fa0d98c777a8174281b5
sallykiddo <salva7099@gmail.com>
2019-09-17 13:22:17 +0200

    update

--- js/index.js
+++ js/index.js
@@ -97,19 +97,3 @@ function openBrowser(url) {
 
 
 
-
-
-
-function test(){
-var url='https://placering.000webhostapp.com';
-openBrowser(url);
-}
-function openBrowser(url) {
- var target = '_blank';
- var options = "location=no"
- var ref = cordova.InAppBrowser.open(url, target, options);
-}
-
-
-
-
